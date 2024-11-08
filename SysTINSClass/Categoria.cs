9707bdd00958fbb80b3ee340ad3b849526745911
Natan1225 <bezerradasilvanatan@gmail.com>
2024-11-08 16:52:16 -0300

    Classes Corrigidas de Casa

--- SysTINSClass/Categoria.cs
+++ SysTINSClass/Categoria.cs
@@ -1,7 +1,6 @@
-﻿using Mysqlx.Prepare;
+﻿
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +9,6 @@ namespace SysTINSClass
 {
     public class Categoria
     {
-        
 
         public int Id { get; set; }
         public string? Nome { get; set; }
@@ -19,24 +17,26 @@ namespace SysTINSClass
         {
 
         }
-
+        public Categoria(string? nome, string? sigla)
+        {
+            Nome = nome;
+            Sigla = sigla;
+        }
         public Categoria(int id, string? nome, string? sigla)
         {
             Id = id;
             Nome = nome;
             Sigla = sigla;
         }
-
         public void Inserir()
         {
             var cmd = Banco.Abrir();
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.CommandText = " sp_categoria_insert";
+            cmd.CommandText = "sp_categoria_insert";
             cmd.Parameters.AddWithValue("spnome", Nome);
             cmd.Parameters.AddWithValue("spsigla", Sigla);
             cmd.ExecuteNonQuery();
             cmd.Connection.Close();
-
         }
         public static Categoria ObterPorId(int id)
         {
@@ -51,15 +51,23 @@ namespace SysTINSClass
                     dr.GetString(1),
                     dr.GetString(2)
                     );
-
-
             }
             return categoria;
         }
-
-        public static List<Categoria> ObterPorLista()
+        public static List<Categoria> ObterLista()
         {
             List<Categoria> categorias = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select * from categorias order by nome asc";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                categorias.Add(new(
+                    dr.GetInt32(0),
+                    dr.GetString(1),
+                    dr.GetString(2)
+                    ));
+            }
             return categorias;
         }
         public bool Atualizar()
@@ -67,31 +75,26 @@ namespace SysTINSClass
             bool resposta = false;
             var cmd = Banco.Abrir();
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("spid",Id);
+            cmd.CommandText = "sp_categoria_update";
+            cmd.Parameters.AddWithValue("spid", Id);
             cmd.Parameters.AddWithValue("spnome", Nome);
             cmd.Parameters.AddWithValue("spsigla", Sigla);
-         if(cmd.ExecuteNonQuery()> 0)
+            if (cmd.ExecuteNonQuery() > 0)
             {
                 cmd.Connection.Close();
                 resposta = true;
             }
             return resposta;
-            
-            
         }
-
-
         public void Excluir()
         {
             var cmd = Banco.Abrir();
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
-
+            cmd.CommandText = "sp_categoria_delete";
             cmd.Parameters.AddWithValue("spid", Id);
-            cmd.CommandText = " sp_categoria_delete";
             cmd.ExecuteNonQuery();
             cmd.Connection.Close();
         }
+
     }
-
-
 }
--- SysTINSClass/Produto.cs
+++ SysTINSClass/Produto.cs
@@ -6,31 +6,29 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SysTINSClass
-    ///começando a classe produto
 {
     public class Produto
     {
         public int Id { get; set; }
         public string? CodBar { get; set; }
-        public string? Descricao{ get; set;}
-         public double ValorUnit { get; set; }
+        public string? Descricao { get; set; }
+        public double ValorUnit { get; set; }
         public string? UnidadeVenda { get; set; }
         public Categoria Categoria { get; set; }
         public double EstoqueMinimo { get; set; }
         public double ClasseDesconto { get; set; }
         public DateTime DataCad { get; set; }
+        public List<Endereco> Enderecos { get; set; }
 
-        
         public Produto()
         {
             Categoria = new();
         }
 
-       
 
-        public Produto(int id, string codBar, string? descricao, double valorUnit, string? unidadeVenda, Categoria categoria, double estoqueMinimo, double classeDesconto, DateTime dataCad)
+
+        public Produto(string codBar, string? descricao, double valorUnit, string? unidadeVenda, Categoria categoria, double estoqueMinimo, double classeDesconto, DateTime dataCad)
         {
-
             CodBar = codBar;
             Descricao = descricao;
             ValorUnit = valorUnit;
@@ -38,9 +36,7 @@ namespace SysTINSClass
             Categoria = categoria;
             EstoqueMinimo = estoqueMinimo;
             ClasseDesconto = classeDesconto;
-
         }
-
         public Produto(int id, string codBar, string? descricao, double valorUnit, string? unidadeVenda, Categoria categoria, double estoqueMinimo, double classeDesconto, DateTime dataCad)
         {
             Id = id;
@@ -54,66 +50,11 @@ namespace SysTINSClass
             DataCad = dataCad;
         }
 
-        
         public void Inserir()
         {
             var cmd = Banco.Abrir();
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.CommandText = " sp_produto_insert";
-            cmd.Parameters.AddWithValue("spcod_barras", CodBar);
-            cmd.Parameters.AddWithValue("spdescricao", Descricao);
-            cmd.Parameters.AddWithValue("spvalor_unit", ValorUnit);
-            cmd.Parameters.AddWithValue("spunidade_venda", UnidadeVenda);
-            cmd.Parameters.AddWithValue("spcategoria_id", Categoria.Id);
-            cmd.Parameters.AddWithValue("spestoque_minimo", EstoqueMinimo);
-            cmd.Parameters.AddWithValue("spclasse_desconto", ClasseDesconto);
-            Id = Convert.ToInt32 (cmd.ExecuteScalar());
-            cmd.Connection.Close();
-
-        }
-        public static Produto ObterPorId(int id)
-        {
-            Produto categoria = new();
-            var cmd = Banco.Abrir();
-            cmd.CommandText = $"select * from categorias where id = {id}";
-            var dr = cmd.ExecuteReader();
-            while (dr.Read())
-            {
-                categoria = new(
-                    dr.GetString(0),
-                    dr.GetString(1),
-                    dr.GetString(2),
-                     dr.GetDouble(3),
-                      dr.GetString(4)),
-                      Categoria.ObterPorId(dr.GetInt32(5)
-                      );
-                      
-            }
-            return categoria;
-
-            //usuario = new(
-               //     dr.GetInt32(0),
-               // dr.GetString(1),
-               // dr.GetString(2),
-               // dr.GetString(3),
-               // Nivel.ObterPorId(dr.GetInt32(4)),
-               // dr.GetBoolean(5)
-               //);
-        }
-
-        public static List<Produto> ObterPorLista()
-        {
-            List <Categoria>  categorias = new();
-
-            return categorias;
-        }
-
-        public bool Atualizar()
-        {
-
-            var cmd = Banco.Abrir();
-            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.CommandText = " sp_produto_update";
+            cmd.CommandText = "sp_produto_insert";
             cmd.Parameters.AddWithValue("spcod_barras", CodBar);
             cmd.Parameters.AddWithValue("spdescricao", Descricao);
             cmd.Parameters.AddWithValue("spvalor_unit", ValorUnit);
@@ -123,33 +64,81 @@ namespace SysTINSClass
             cmd.Parameters.AddWithValue("spclasse_desconto", ClasseDesconto);
             Id = Convert.ToInt32(cmd.ExecuteScalar());
             cmd.Connection.Close();
-            if(cmd.ExecuteReader)
-
-
         }
-
-
+        public static Produto ObterPorId(int id)
+        {
+            Produto produto = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select * from produtos where id = {id}";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                produto = new(
+                   dr.GetInt32(0),
+                   dr.GetString(1),
+                   dr.GetString(2),
+                   dr.GetDouble(3),
+                   dr.GetString(4),
+                   Categoria.ObterPorId(dr.GetInt32(5)),
+                   dr.GetDouble(6),
+                   dr.GetDouble(7),
+                   dr.GetDateTime(9)
+                    );
+            }
+            return produto;
+        }
+        public static List<Produto> ObterLista()
+        {
+            List<Produto> produtos = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select * from produtos order by descricao asc";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                produtos.Add(new(
+                   dr.GetInt32(0),
+                   dr.GetString(1),
+                   dr.GetString(2),
+                   dr.GetDouble(3),
+                   dr.GetString(4),
+                   Categoria.ObterPorId(dr.GetInt32(5)),
+                   dr.GetDouble(6),
+                   dr.GetDouble(7),
+                   dr.GetDateTime(9)
+                    ));
+            }
+            return produtos;
+        }
+        public bool Atualizar()
+        {
+            bool resposta = false;
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_produto_update";
+            cmd.Parameters.AddWithValue("spid", Id);
+            cmd.Parameters.AddWithValue("spcod_barras", CodBar);
+            cmd.Parameters.AddWithValue("spdescricao", Descricao);
+            cmd.Parameters.AddWithValue("spvalor_unit", ValorUnit);
+            cmd.Parameters.AddWithValue("spunidade_venda", UnidadeVenda);
+            cmd.Parameters.AddWithValue("spcategoria_id", Categoria.Id);
+            cmd.Parameters.AddWithValue("spestoque_minimo", EstoqueMinimo);
+            cmd.Parameters.AddWithValue("spclasse_desconto", ClasseDesconto);
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                cmd.Connection.Close();
+                resposta = true;
+            }
+            return resposta;
+        }
         public void Excluir()
         {
             var cmd = Banco.Abrir();
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
-
+            cmd.CommandText = "sp_produto_delete";
             cmd.Parameters.AddWithValue("spid", Id);
-            cmd.CommandText = " sp_categoria_delete";
             cmd.ExecuteNonQuery();
             cmd.Connection.Close();
         }
 
-
-
-
     }
-
-    
-}     
-
- 
-   
-
-   
-
+}
